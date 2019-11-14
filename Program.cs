822aef9161ca136ccc1b508d3e5bef9040d18aff
kierepkae <kierepkaedyta@gmail.com>
2019-11-14 13:40:02 +0000

    define enum ShippingMethod

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
 using System;
 
 namespace StringPractice {
+  public enum ShippingMethod {
+    RegularAirMail,
+    RegisteredAirMail,
+    Express
+  }
+
   class Program {
     static void Main (string[] args) {
 
